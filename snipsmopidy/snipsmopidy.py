79ca493471b807780ecef3205bb38e7d1e6c87ab
paufabregat <pau.fabregat.p@gmail.com>
2018-03-22 19:22:34 +0100

    Refactor using Mopidy http server endpoint

--- snipsmopidy/snipsmopidy.py
+++ snipsmopidy/snipsmopidy.py
@@ -3,194 +3,309 @@
 
 from __future__ import unicode_literals
 
-from mpd import MPDClient
-from mpd import ConnectionError
-from spotify import SpotifyClient
+import requests
+import mopidy.models
+import json
+from requests import ConnectionError
 import time
 
 MAX_VOLUME = 100
 GAIN = 4
-MPD_PORT = 6600
+HTTP_PORT = 6680
+MOPIDY_RELATIVE = '/mopidy/rpc'
 
 
 class SnipsMopidy:
     """Mopidy skill for Snips.
 
-    :param mopidy_host: The hostname of the Mopidy player
+    :param mopidy_host: The hostname of the Mopidy http server
     """
 
-    def __init__(self, spotify_refresh_token=None, spotify_client_id=None, spotify_client_secret=None,
-                 mopidy_host='127.0.0.1', locale=None):
+    def __init__(self, mopidy_host='127.0.0.1', locale=None):
         self.host = mopidy_host
+        self.url = "http://{}:{}{}".format(mopidy_host, HTTP_PORT, MOPIDY_RELATIVE)
         connexion_established = False
         while not connexion_established:
-            self.client = MPDClient()
             try:
-                self.client.connect(self.host, MPD_PORT)
+                self.get_current_track()
                 connexion_established = True
-                break
-            except Exception as ex:
+            except ConnectionError:
                 print("Mopidy is not yet available, please wait or be sure it is running typing: "
                       "systemctl status mopidy")
                 print("...")
                 time.sleep(5)
 
-        status = self.client.status()
-        self.previous_volume = int(status.get('volume'))
+        self.previous_volume = self.get_volume()
         self.max_volume = MAX_VOLUME
-        if spotify_refresh_token is not None:
-            self.spotify = SpotifyClient(spotify_refresh_token, spotify_client_id, spotify_client_secret)
+
+    def run(self, method, params=None):
+        json_rpc = {
+            'jsonrpc': '2.0',
+            'id': 1,
+            'method': method
+        }
+
+        if params is not None:
+            json_rpc['params'] = params
+
+        response = requests.post(
+            self.url,
+            data=json.dumps(
+                json_rpc,
+                cls=mopidy.models.ModelJSONEncoder
+            )
+        )
+
+        decoded = json.loads(
+            response.text,
+            object_hook=mopidy.models.model_json_decoder
+        )
+
+        if 'error' in decoded:
+            print(decoded['error'])
+            return None
+        return decoded['result']
+
+    def get_current_track(self):
+        return self.run('core.playback.get_current_track')
+
+    def get_tl_tracks(self):
+        return self.run('core.tracklist.get_tl_tracks')
+
+    def play(self, tl_track=None, tlid=None):
+        return self.run(
+            'core.playback.play',
+            {
+                'tl_track': tl_track,
+                'tlid': tlid
+            }
+        )
+
+    def play_first(self):
+        tracklist = self.get_tl_tracks()
+        return self.play_tlid(tracklist[0].tlid)
+
+    def play_tlid(self, tlid):
+        return self.play(tlid=tlid)
 
     def pause(self):
-        self.client.pause(1)
-
-    def volume_up(self, level):
-        if self.client is None:
-            return
-        level = int(level)*10 if level is not None else 10
-        status = self.client.status()
-        current_volume = int(status.get('volume'))
-        self.client.setvol(min(
-            current_volume + GAIN * level,
-            self.max_volume))
-        if status.get('state') != 'play':
-            self.client.play()
-
-    def volume_down(self, level):
-        if self.client is None:
-            return
-        level = int(level)*10 if level is not None else 10
-        status = self.client.status()
-        current_volume = int(status.get('volume'))
-        self.client.setvol(current_volume - GAIN * level)
-        if status.get('state') != 'play':
-            self.client.play()
-
-    def set_volume(self, volume_value):
-        if self.client is None:
-            return
-        self.client.setvol(volume_value)
-        self.client.play()
-
-    def set_to_low_volume(self):
-        try:
-            if self.client is None:
-                return
-            status = self.client.status()
-            if status.get('state') != 'play':
-                return None
-            current_volume = int(status.get('volume'))
-            self.previous_volume = current_volume
-            self.client.setvol(min(30, current_volume))
-            if status.get('state') != 'play':
-                self.client.play()
-        except ConnectionError:
-            print("Connection Error. Trying to reconnect with mpd")
-            self.client.connect(self.host, MPD_PORT)
-            self.set_to_low_volume()
-
-    def set_to_previous_volume(self):
-        if self.client is None:
-            return
-        if self.previous_volume is None:
-            return None
-        self.client.setvol(self.previous_volume)
-        status = self.client.status()
-        if status.get('state') != 'play':
-            self.client.play()
+        return self.run('core.playback.pause')
 
     def stop(self):
-        if self.client is None:
-            return
-        self.client.stop()
+        return self.run('core.playback.stop')
 
-    def play_playlist(self, name, _shuffle=False):
-        if self.client is None:
-            return None
-        if self.spotify is None:
-            return None
-        tracks = self.spotify.get_playlist(name)
-        if tracks is None:
-            return None
-        self.client.stop()
-        self.client.clear()
-        for track in tracks:
-            try:
-                self.client.add(track['track']['uri'])
-            except Exception:
-                print("Song not available in catalogue")
-        if _shuffle:
-            self.client.shuffle()
-        self.client.play()
+    def get_status(self):
+        """
+        Get the current state of the playback
+        :return: String either playing, paused, or stopped
+        """
+        return self.run('core.playback.get_state')
 
-    def play_artist(self, name):
-        if self.client is None:
-            return None
-        if self.spotify is None:
-            return None
-        tracks = self.spotify.get_top_tracks_from_artist(name)
-        if tracks is None:
-            return None
-        self.client.stop()
-        self.client.clear()
-        for track in tracks:
-            self.client.add(track['uri'])
-        self.client.play()
+    def set_volume(self, percent):
+        return self.run(
+            'core.mixer.set_volume',
+            {'volume': percent}
+        )
 
-    def play_album(self, album, _shuffle=False):
-        if self.client is None:
-            return
-        if self.spotify is None:
-            return
-        tracks = self.spotify.get_tracks_from_album(album)
-        if tracks is None:
+    def get_volume(self):
+        return self.run('core.mixer.get_volume')
+
+    def volume_up(self):
+        volume = self.get_volume()
+        if volume is None:
+            self.set_volume(50)
+        else:
+            self.set_volume(min(100, volume + 10))
+        status = self.get_status()
+        if status != 'playing':
+            self.play()
+
+    def volume_down(self):
+        volume = self.get_volume()
+        if volume is None:
+            self.set_volume(50)
+        else:
+            self.set_volume(max(0, volume - 10))
+        status = self.get_status()
+        if status != 'playing':
+            self.play()
+
+    def set_to_low_volume(self):
+        status = self.get_status()
+        if status != 'playing':
             return None
-        self.client.stop()
-        self.client.clear()
-        for track in tracks:
-            self.client.add(track['uri'])
-        if _shuffle:
-            self.client.shuffle()
-        self.client.play()
+        current_volume = self.get_volume()
+        print(current_volume)
+        self.previous_volume = current_volume
+        self.set_volume(min(30, current_volume))
+        current_volume = int(self.get_volume())
+        print(current_volume)
+        self.play()
 
-    def play_song(self, name):
-        if self.client is None:
-            return
-        if self.spotify is None:
-            return
-        track = self.spotify.get_track(name)
-        if track is None:
+    def set_to_previous_volume(self):
+        if self.previous_volume is None:
             return None
-        self.client.stop()
-        self.client.clear()
-        self.client.add(track['uri'])
-        self.client.play()
+        self.set_volume(self.previous_volume)
+        status = self.get_status()
+        if status != 'playing':
+            self.play()
 
-    def play_next_item_in_queue(self):
-        if self.client is None:
-            return
-        try:
-            self.client.next()
-        except Exception:
-            print("Failed to play next item, maybe last song?")
+    def next_song(self):
+        return self.run('core.playback.next')
 
-    def play_previous_item_in_queue(self):
-        if self.client is None:
-            return
-        try:
-            self.client.previous()
-        except Exception:
-            print("Failed to play previous item, maybe first song?")
+    def previous_song(self):
+        return self.run('core.playback.previous')
+
+    def mute(self):
+        mute_state = self.run('core.mixer.get_mute')
+
+        if mute_state is None:
+            mute_state = True
+
+        return self.run(
+            'core.mixer.set_mute',
+            {'mute': not mute_state}
+        )
+
+    def shuffle(self):
+        return self.run('core.tracklist.shuffle')
+
+    def search(self, parameters):
+        return self.run('core.library.search', parameters)
+
+    def search_spotify(self, parameters):
+        spotify = parameters.copy()
+        spotify['uris'] = ['spotify:']
+        return self.search(spotify)
+
+    def search_any(self, search_term):
+        return self.search_spotify({'any': [search_term]})
+
+    def search_genre(self, genre):
+        return self.search_spotify({'genre': [genre]})
+
+    def search_artist(self, artist):
+        return self.search_spotify({'artist': [artist]})
+
+    def search_album(self, album):
+        search_results = self.search_spotify({'album': [album]})
+        album_uri = search_results[0].albums[0].uri
+        return self.search_spotify({'uri': [album_uri]})
+
+    def search_track(self, track):
+        return self.search_spotify({'track_name': [track]})
+
+    def add_to_tracklist(self, tracks=None, uris=None):
+        if tracks is not None:
+            return self.run(
+                'core.tracklist.add',
+                {'tracks': tracks}
+            )
+        elif uris is not None:
+            return self.run(
+                'core.tracklist.add',
+                {'uris': uris}
+            )
+        else:
+            return None
+
+    def clear_tracklist(self):
+        return self.run('core.tracklist.clear')
+
+    def set_tracks(self, new_tracks):
+        if 0 < len(new_tracks):
+            self.clear_tracklist()
+            self.pause()
+            self.add_to_tracklist(tracks=new_tracks)
+            return True
+        else:
+            print('Empty tracklist')
+            return False
+
+    def set_uris(self, new_track_uris):
+        if 0 < len(new_track_uris):
+            self.clear_tracklist()
+            self.pause()
+            self.add_to_tracklist(uris=new_track_uris)
+            return True
+        else:
+            print('Empty tracklist')
+            return False
+
+    def set_search(self, search_results):
+        tracks = [
+            track
+            for result in search_results
+            for track in result.tracks
+        ]
+        return self.set_tracks(tracks)
+
+    def find_similar_playlist(self, name):
+        # TODO send to TTS bus
+        print("No playlists found, trying to find a similar playlist...".format(name))
+        return self.set_search(music.search_genre(name))
+
+    def search_playlist(self, name):
+        playlists = self.run('core.playlists.as_list')
+
+        if playlists is None:
+            return self.find_similar_playlist(name)
+        playlists = filter(
+            lambda playlist: playlist.name.lower().startswith(name.lower()),
+            playlists
+        )
+        return playlists
+
+    def set_playlist(self, playlists, name):
+        if 0 == len(playlists):
+            return self.find_similar_playlist(name)
+
+        refs = self.run(
+            'core.playlists.get_items',
+            {'uri': playlists[0].uri}
+        )
+
+        return self.set_uris([ref.uri for ref in refs])
 
     def get_info(self):
         # Get info about currently playing tune
-        info = self.client.currentsong()
-        return info['title'], info['artist'], info['album']
+        track = self.get_current_track()
+        title = track.name
+        artist = [artist.name for artist in track.album.artists][0]
+        album = track.album.name
+        return title, artist, album
 
-    def add_song(self):
-        # Save song in spotify
-        info = self.client.currentsong()
-        self.spotify.add_song(info['artist'], info['title'])
+    def play_song(self, name, shuffle=False):
+        is_set = self.set_search(self.search_track(name))
+        if is_set:
+            if shuffle:
+                self.shuffle()
+            self.play_first()
 
-    def play(self):
-        self.client.play()
+    def play_album(self, name, shuffle=False):
+        is_set = self.set_search(self.search_album(name))
+        if is_set:
+            if shuffle:
+                self.shuffle()
+            self.play_first()
+
+    def play_artist(self, name, shuffle=False):
+        is_set = self.set_search(self.search_artist(name))
+        if is_set:
+            if shuffle:
+                self.shuffle()
+            self.play_first()
+
+    def play_playlist(self, name, shuffle=False):
+        is_set = self.set_playlist(self.search_playlist(name), name)
+        if is_set:
+            if shuffle:
+                self.shuffle()
+            self.play_first()
+
+    def play_genre(self, name, shuffle=False):
+        is_set = self.set_search(self.search_artist(name))
+        if is_set:
+            if shuffle:
+                self.shuffle()
+            self.play_first()

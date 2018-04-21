bf326a9fcca6edfd6f10fd209dc3618f63721809
Hugh Cole-Baker <sigmaris@gmail.com>
2018-04-21 01:24:24 +0100

    Add missing packages

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     url='https://github.com/snipsco/snips-skill-mopidy',
     download_url='',
     license='MIT',
-    install_requires=['python-mpd2'],
+    install_requires=['python-mpd2', 'fuzzywuzzy', 'python-Levenshtein'],
     test_suite="tests",
     keywords=['snips', 'mopidy'],
     packages=['snipsmopidy'],
